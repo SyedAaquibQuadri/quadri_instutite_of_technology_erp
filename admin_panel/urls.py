c7c2c07f616d5dec805dcde556ceda31ecec4425
SyedAaquibQuadri <syedaaquibquadri@gmail.com>
2026-06-10 14:13:31 +0530

    TICKET-007  DONE  Base sidebar layout template

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+app_name = 'admin_panel'
+
+urlpatterns = [
+    path('dashboard/', views.dashboard, name='dashboard'),
+]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from accounts.decorators import admin_required
 # Create your views here.
+@admin_required
+def dashboard(request):
+    return render(request, 'admin_panel/dashboard.html')
